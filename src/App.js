01e6cc3b60fe840f54f0982adbc1fcb97634b0f9
IkramMissaoui <missaoui.ikram726@gmail.com>
2019-05-28 10:15:41 +0200

    Create a React app and publish it to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Card from './component/card.js';
+import './App.css'
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <Card/>
+     );
+  }
+}
+ 
+export default App;
