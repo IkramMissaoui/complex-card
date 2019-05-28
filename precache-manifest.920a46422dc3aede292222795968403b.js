90ee8652c4abccde90b316a80c238830c697da39
IkramMissaoui <missaoui.ikram726@gmail.com>
2019-05-28 10:13:51 +0200

    Updates

---
+++ precache-manifest.920a46422dc3aede292222795968403b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186c1005db0ecc26063caa8955333932",
+    "url": "/complex-card/index.html"
+  },
+  {
+    "revision": "2108ef6086394e2325d0",
+    "url": "/complex-card/static/css/main.7608103b.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/complex-card/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "2108ef6086394e2325d0",
+    "url": "/complex-card/static/js/main.3ae782c7.chunk.js"
+  },
+  {
+    "revision": "dc6d3cdb62f915f3e6ee",
+    "url": "/complex-card/static/js/runtime~main.5823846f.js"
+  },
+  {
+    "revision": "2bd8653315acb7c8ae4c485f34984c29",
+    "url": "/complex-card/static/media/back.2bd86533.jpg"
+  },
+  {
+    "revision": "4bfd75adb187034c3460d2907ac2bfe1",
+    "url": "/complex-card/static/media/imb.4bfd75ad.png"
+  },
+  {
+    "revision": "d3c2603be59e2211e9a52ad8e774a14e",
+    "url": "/complex-card/static/media/mcard.d3c2603b.png"
+  }
+]);
